053fec0105a5138002eb7f16142fd38f0879e4c6
yeyifu <1437552152@qq.com>
2018-12-31 00:14:16 +0800

    登录验证

---
+++ src/views/home/home.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="home">
+	<h5>智慧校园管理平台系统</h5>
+	</div>
+</template>
+
+<script> 
+
+export default {
+		data() {
+			return {
+		
+		}	
+	}}
+</script>
+<style lang="less" scoped>
+h5{
+	font-size: 40px;
+    color: #2d8cf0;
+    text-align: center;
+    margin-top: 250px;
+}
+</style>
